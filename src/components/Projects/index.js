32cddb70821458431f1753453ea5bbe89faedd9f
Allyson Nasworthy <allyson.nasworthy@gmail.com>
2022-07-28 18:41:55 -0400

    project

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -5,6 +5,7 @@ import mydreampassport from '../../assets/images/mydreampassport.png';
 import budget from '../../assets/images/budgettracker.PNG';
 import ecommerce from '../../assets/images/ecommerce.PNG';
 import weather from '../../assets/images/WeatherDashScreenshot.PNG';
+import finders from '../../assets/images/fk3.PNG';
 
 const Projects = () => {
 
@@ -45,6 +46,12 @@ return (
                 <h2>Ecommerce Back End</h2>
                 <p>Find the Repo <a href='https://github.com/awnasworthy/ecommerce' target="_blank">here!</a> </p>
                 <p>See GitHub for demo video!</p>
+            </div>
+            <div>
+                <img src={finders}/>
+                <h2>Finders Keepers</h2>
+                <p>Find the Repo <a href='https://github.com/NicoleSharrock/finders-keepers' target="_blank">here!</a> </p>
+                <p>Project Deployed <a href='https://morning-garden-68781.herokuapp.com/' target="_blank">here!</a></p>
             </div>
     </div>
 )
